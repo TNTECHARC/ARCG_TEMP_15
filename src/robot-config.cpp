dca307bc968fe6da9acb1241c41de6060bc8c346
erbadger27-create <erbadger27@gmail.com>
2025-11-10 21:13:02 -0600

    update

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -10,10 +10,14 @@ controller Controller1;
 
 //////////// Drive Train Motors ////////////
 
-motor R1 = motor(PORT20, ratio6_1, false);
-motor R2 = motor(PORT10, ratio6_1, false);
-motor L1 = motor(PORT16, ratio6_1, true);
-motor L2 = motor(PORT1, ratio6_1, true);
+motor R1 = motor(PORT12, ratio6_1, false);
+motor R2 = motor(PORT11, ratio6_1, false);
+motor R3 = motor(PORT1, ratio6_1, false);
+motor R3 = motor(PORT2, ratio6_1, false);
+motor L1 = motor(PORT19, ratio6_1, true);
+motor L2 = motor(PORT20, ratio6_1, true);
+motor L3 = motor(PORT10, ratio6_1, true);
+motor L4 = motor(PORT9, ratio6_1, true);
 
 ///////////////////////////////////////////
 
